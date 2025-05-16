95247d75171b264aab1498ecf07786cd18de9df3
Petar S. <78117607+stolefx@users.noreply.github.com>
2025-05-16 20:41:20 +0200

    TableRow component

--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,30 +1,13 @@
 import Heading from "../components/Heading"
-import Menu from "../components/Menu"
+import TableRow from "../components/TableRow"
 
 const ReportPage = () => {
   return (
     <div>
         <Heading title={"Izvestaj"} />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio perspiciatis hic exercitationem nihil voluptatibus? Animi porro quo laboriosam magni inventore, sequi at fugit, atque earum quisquam officia ab, iusto iure?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit similique earum saepe nesciunt, delectus tempora exercitationem! Debitis molestias illo aliquam magni quaerat, quam, at deserunt perspiciatis, cumque ratione tempore. Sunt.
-        Modi, ex nostrum harum fugit quae maxime magni. Praesentium officia tenetur, aliquid totam, ad nobis quasi voluptate deserunt ut velit dolor autem iste distinctio cumque tempore aspernatur alias quisquam error.
-        Praesentium autem minus necessitatibus est aliquid? Ipsa officia totam blanditiis vitae dolorem dolorum reprehenderit, at, eveniet magni minus sapiente aperiam voluptatibus consequuntur corporis, alias modi maiores et sint neque. Earum.
-        Ea omnis distinctio qui maiores, ad nihil, aperiam alias, consequuntur quod deleniti accusantium sapiente voluptas! Aliquam iste eius blanditiis tempora sapiente, debitis quibusdam hic, incidunt qui, dolor cupiditate unde sint!
-        Voluptas deleniti facilis quisquam nam sit repudiandae. Praesentium voluptas nostrum fugiat, rerum debitis quia quos modi aperiam quod in numquam cum laborum nemo dolore ad fugit itaque obcaecati blanditiis aliquid.
-        Necessitatibus quis aliquid blanditiis deleniti fugit possimus nam reprehenderit quos harum dignissimos commodi corporis velit, iusto impedit ad est optio vero dicta! Nisi architecto dignissimos cum, sapiente quas odio libero?
-        Consectetur ex repudiandae odit omnis, voluptatem magnam tenetur molestiae recusandae excepturi quaerat dicta nesciunt, ullam, deleniti accusantium dolores! Voluptatum corrupti beatae labore iusto dolor placeat, numquam aperiam a vitae adipisci!
-        Eaque assumenda quibusdam molestiae tenetur, maxime laboriosam odit sed, blanditiis obcaecati, natus libero labore quasi aliquam voluptatem dicta architecto. Mollitia a doloribus fugiat exercitationem libero placeat, iusto cupiditate ex excepturi.
-        Odit reiciendis doloremque accusamus doloribus culpa, numquam ratione earum unde quia, est minima iste ipsum veniam perspiciatis, voluptatibus at dolore esse dolorum. Dicta totam rerum similique? Iure sint asperiores harum.
-        Voluptate odio expedita laudantium fugit, asperiores, ad quidem, tenetur non sunt exercitationem et ipsam eaque dolor incidunt. A voluptatibus sint aut repudiandae exercitationem eum enim beatae ea. Architecto, voluptate eveniet!
-        Sunt, deserunt voluptate iste vel eaque quia quibusdam culpa quos possimus? Ut laborum odit sint voluptatibus quod quis nisi, dicta, quos, quam similique itaque earum officiis dignissimos aspernatur non exercitationem?
-        Rerum distinctio recusandae voluptatibus, unde architecto dicta qui minima quae, itaque cum beatae eligendi ullam aperiam dolore sed doloremque facere excepturi amet ipsum pariatur nemo tempore. Voluptates ad reiciendis voluptate!
-        Corrupti eos, sequi excepturi atque nam nihil nulla amet est quibusdam non at ratione ex illo praesentium officia temporibus. Consequatur veritatis placeat accusantium maiores sunt alias commodi doloremque itaque fuga?
-        Minima error vitae molestiae voluptate quis beatae nostrum exercitationem, ipsam voluptatibus recusandae placeat voluptates laudantium quo laboriosam iusto doloremque sunt. Quibusdam sunt cupiditate maxime quas molestias necessitatibus iste omnis quam.
-        Debitis quia voluptatum nam neque, quas eum quaerat sit aspernatur. Quod culpa, dicta deserunt beatae amet odit! Mollitia dicta placeat a, provident rem error dolor. Fugit ipsa facere non error.
-        Et optio nemo eaque voluptate praesentium labore nulla sunt, ab quis veritatis ipsam accusantium itaque magnam odio, voluptas adipisci minima, consequatur officiis expedita id nihil non minus cupiditate. Ad, itaque.
-        Similique odit, delectus quod accusamus obcaecati nihil consectetur quisquam, rem reiciendis quos impedit recusandae, autem voluptates suscipit maiores esse assumenda itaque labore ad ipsum fugiat dolorum placeat et. Totam, modi.
-        Fugit consequuntur maxime, neque, vitae officia iure aspernatur iusto dignissimos aperiam magni soluta temporibus saepe libero! Libero aliquid molestiae magni tempora fugit autem esse, cupiditate error nihil nam a voluptas.
-        Optio, odio fugit. Ad vel ratione molestias dignissimos tempore iure laborum dicta delectus quis consequatur ipsa praesentium corrupti, in minima maiores perspiciatis amet, blanditiis sit fuga expedita atque quos sed.</p>
+        
+        <TableRow data={{"Ime": "Marko", "Prezime": "Markovic", "Grad": "Novi Sad"}}/>
     </div>
   )
 }
