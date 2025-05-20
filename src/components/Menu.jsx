1dbb15f92e1ca0ac10193ec95a73ca4a85e371f5
petaaar88 <pe.bn80@gmail.com>
2025-05-20 16:53:48 +0200

    Add Link Routing

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,26 @@
+import { NavLink } from 'react-router-dom'
 import MenuButton from './MenuButton'
 import ThemeButton from './ThemeButton'
 
 const Menu = () => {
+
+  const navStyle = ({isActive}) =>{
+      return {
+        color: isActive ?"blue" : "white",
+        borderRight: isActive ? "8px solid blue " : ""
+      }
+  }
+
   return (
+
     <nav style={{background:"#252E2D", width:"300px"}} className="h-screen fixed">
         <div className='h-full flex flex-col justify-between'>
           <div>
             <h3 className="text-5xl text-white font-semibold text-center py-7">CRMS</h3>
             <ul>
-              <li><MenuButton text="Home" /></li>
-              <li><MenuButton text="Reports" /></li>
-              <li><MenuButton text="Settings" /></li>
-              <li><MenuButton text="Logout" /></li>
+              <li><NavLink style={navStyle} className={'block w-full text-center py-3 text-lg text-white cursor-pointer hover:bg-gray-700'} to={"/home"}>Home</NavLink></li>
+              <li><NavLink style={navStyle} className={'block w-full text-center py-3 text-lg text-white cursor-pointer hover:bg-gray-700'} to={"/reports"}>Reports</NavLink></li>
+              <li><NavLink style={navStyle} className={'block w-full text-center py-3 text-lg text-white cursor-pointer hover:bg-gray-700'} to={"/settings"}>Settings</NavLink></li>
             </ul>
           </div>
 
--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
 
 const MenuButton = ({text}) => {
     const [isActive, setIsActive] = useState(false);
 
   return (
+    
     <button className='block w-full py-3 text-lg text-white cursor-pointer hover:bg-gray-700'>{text}</button>
   )
 }
