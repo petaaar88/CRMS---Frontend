2bee3819f40d79996983e88ab6e99189f6010898
Petar S. <stosic.p04@gmail.com>
2025-08-08 22:31:23 +0200

    Loading page style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from "react-router-dom"
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
 import ReportPage from "./pages/ReportPage"
 import MainLayout from "./layouts/MainLayout";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -8,28 +8,28 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import AuthorizedRoute from "./components/AuthorizedRoute";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route element={<ProtectedRoute><MainLayout/></ProtectedRoute>}>
-        <Route path="/" element={<p>Neki tekst</p>}/>
-        <Route path="/admin" element={<AuthorizedRoute authorizedRoles={["ADMIN"]}><p>Admin Page</p></AuthorizedRoute>}/>
-        <Route path="/reports" element= {<ReportPage />}/>
-        <Route path="*" element={<p>Not Found!</p>}/>
-      </Route>
-    </Route>
-    
-  )
+    createRoutesFromElements(
+        <Route>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route element={<ProtectedRoute><MainLayout/></ProtectedRoute>}>
+                <Route path="/" element={<p>Neki tekst</p>}/>
+                <Route path="/admin"
+                       element={<AuthorizedRoute authorizedRoles={["ADMIN"]}><p>Admin Page</p></AuthorizedRoute>}/>
+                <Route path="/reports" element={<ReportPage/>}/>
+                <Route path="*" element={<p>Not Found!</p>}/>
+            </Route>
+        </Route>
+    )
 );
 
 const App = () => {
-  return (
-    <ThemeProvider>
-      <AuthProvider>
-        <RouterProvider router={router}/>
-      </AuthProvider>
-    </ThemeProvider>
-  )
+    return (
+            <ThemeProvider>
+                <AuthProvider>
+                    <RouterProvider router={router}/>
+                </AuthProvider>
+            </ThemeProvider>
+    )
 }
 
 export default App
--- src/pages/LoadingPage/LoadingPage.jsx
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -1,10 +1,47 @@
+import {Box, CircularProgress, Typography} from "@mui/material";
+import useTheme from "../../hooks/useTheme";
 
 const LoadingPage = () => {
-  return (
-    <div className="h-full flex flex-col justify-center ">
-        <h1 className="text-5xl text-center">LoadingPage</h1>
-    </div>
-  )
-}
+    const {theme} = useTheme();
+    return (
+        <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 2,
+                backgroundColor:
+                    theme === "dark"
+                        ? "var(--color-deep-green)"
+                        : "var(--color-white)"
+            }}
+        >
+            <CircularProgress
+                size={60}
+                sx={{
+                    color:
+                        theme === "dark"
+                            ? "var(--color-menu-button-dark)"
+                            : "var(--color-button-light-green)"
+                }}
+            />
 
-export default LoadingPage
+            <Typography
+                variant="h5"
+                align="center"
+                sx={{
+                    color:
+                        theme === "dark"
+                            ? "var(--color-white)"
+                            : "var(--color-black)"
+                }}
+            >
+                Loading
+            </Typography>
+        </Box>
+    );
+};
+
+export default LoadingPage;
